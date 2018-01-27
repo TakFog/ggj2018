14ed087d3117d081b1182221e6c288298a3f7069
TakFog <takfog@gmail.com>
2018-01-27 08:29:02 +0100

    draw logic

--- GGJ_Backend/Assets/Scripts/GameLogic.cs
+++ GGJ_Backend/Assets/Scripts/GameLogic.cs
@@ -6,11 +6,34 @@ public enum BodyPart {Head, Chest, Legs };
 
 public class Card
 {
+    public const int MAX_ATTRIBUTE = 10;
+
     public BodyPart part;
     public int attack;
     public int defence;
     public float speed;
     public int graphictype;
+
+    public void GenerateAttributes()
+    {
+        if (part == BodyPart.Legs)
+        {
+            defence = 0;
+            if (speed == 0)
+            {
+                attack = 0;
+            }
+            else
+            {
+                attack = MAX_ATTRIBUTE;
+            }
+        }
+        else
+        {
+            attack = Random.Range(0, MAX_ATTRIBUTE + 1);
+            defence = MAX_ATTRIBUTE - attack;
+        }
+    }
 }
 
 public class Golem
--- GGJ_Backend/Assets/Scripts/Hand.cs
+++ GGJ_Backend/Assets/Scripts/Hand.cs
@@ -5,13 +5,139 @@ using UnityEngine;
 public class Hand : MonoBehaviour {
     public HandSlot[] slots;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static Hand inst;
+    public static Hand Instance
+    {
+        get { return inst; }
+        private set { inst = value; }
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Damage()
+    {
+        int i = slots.Length - 1;
+        while (i>=0 && !slots[i].SlotEnabled)
+        {
+            i--;
+        }
+        slots[i].SlotEnabled = false;
+        i--;
+        for(; i>=0; i--)
+        {
+            slots[i].Card = null;
+        }
+        //gestire tempi di animazione
+        Deck.Instance.DrawCard();
+    }
+
+    public void AddCard()
+    {
+        if (!Deck.Instance.DrawCard()) return;
+        int heads = 0, chests = 0, legs = 0, empty = 0;
+        for(int i=0; i<slots.Length; i++)
+        {
+            HandSlot slot = slots[i];
+            if (slot.SlotEnabled)
+            {
+                Card c = slot.Card;
+                if (c == null)
+                {
+                    empty++;
+                }
+                else
+                {
+                    switch (c.part)
+                    {
+                        case BodyPart.Head:
+                            heads++;
+                            break;
+                        case BodyPart.Chest:
+                            chests++;
+                            break;
+                        case BodyPart.Legs:
+                            legs++;
+                            break;
+                    }
+                }
+            }
+        }
+
+        BodyPart newPart;
+
+        switch (empty)
+        {
+            case 0:
+                return;
+            case 1:
+                if (heads == 0)
+                    newPart = BodyPart.Head;
+                else if (chests == 0)
+                    newPart = BodyPart.Chest;
+                else if (legs == 0)
+                    newPart = BodyPart.Legs;
+                else
+                    newPart = randomPart();
+                break;
+            case 2:
+                if (heads == 0)
+                {
+                    if (chests == 0)
+                        newPart = randomPart(BodyPart.Head, BodyPart.Chest);
+                    else if (legs == 0)
+                        newPart = randomPart(BodyPart.Head, BodyPart.Legs);
+                    else
+                        newPart = randomPart();
+                }
+                else if (chests == 0 && legs == 0)
+                    newPart = randomPart(BodyPart.Chest, BodyPart.Legs);
+                else
+                    newPart = randomPart();
+                break;
+            default:
+                newPart = randomPart();
+                break;
+        }
+
+        Card card = new Card();
+        card.part = newPart;
+        CardDatabase.Instance.RandomGraphic(card);
+        card.GenerateAttributes();
+
+        for(int i=0; i<slots.Length; i++)
+        {
+            if (slots[i].IsFree)
+            {
+                slots[i].Card = card;
+                break;
+            }
+               
+        }
+    }
+
+    private BodyPart randomPart()
+    {
+        switch (Random.Range(0, 3))
+        {
+            case 0:
+                return BodyPart.Head;
+            case 1:
+                return BodyPart.Chest;
+            case 2:
+                return BodyPart.Legs;
+        }
+        return BodyPart.Head;
+    }
+
+    private BodyPart randomPart(BodyPart p1, BodyPart p2)
+    {
+        if (Random.Range(0, 2) == 0)
+            return p1;
+        else
+            return p2;
+    }
 }
