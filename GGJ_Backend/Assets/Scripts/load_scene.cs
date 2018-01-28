6eabbabb2ce302c3abe724983d78969de02d2b02
Giorgio <giorgiomariani94@gmail.com>
2018-01-28 03:43:29 +0100

    aggiunto scena che carica livello

---
+++ GGJ_Backend/Assets/Scripts/load_scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load_scene : MonoBehaviour {
+
+    public void loadScene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+}
