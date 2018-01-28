6b5d7e9b9d1053e6ea32a3d7600f38cb4b5e9f81
Giorgio <giorgiomariani94@gmail.com>
2018-01-28 10:44:22 +0100

    added scenes

--- GGJ_Backend/Assets/Scripts/load_scene.cs
+++ GGJ_Backend/Assets/Scripts/load_scene.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class load_scene : MonoBehaviour {
 
-    public void loadScene(string s)
+    public void LoadScene(string s)
     {
         SceneManager.LoadScene(s);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
     }
 }
