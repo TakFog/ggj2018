aa5382574e262ff855cad565f5c3057262a4e8cf
TakFog <takfog@gmail.com>
2018-01-31 20:55:57 +0100

    ip address in title screen

---
+++ GGJ_Backend/Assets/Scripts/IPShow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IPShow : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        System.Net.IPAddress[] a =
+            System.Net.Dns.GetHostAddresses(System.Net.Dns.GetHostName());
+        string ip = a[0].ToString();
+        for(int i=1; i<a.Length; i++)
+        {
+            if (a[i].AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                ip = a[i].ToString();
+        }
+        GetComponent<Text>().text = "IP: " + ip;
+        this.enabled = false;
+    }
+	
+}
